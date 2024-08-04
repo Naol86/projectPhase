9a747f92ac68536fa367d730d1ec49d1fb7905d1
naol86 <naol.kasinet@a2sv.org>
2024-08-04 15:45:41 +0300

    woking with task 7

--- test-7b/src/pages/ErrorPage.tsx
+++
@@ -1,7 +0,0 @@
-export default function ErrorPage() {
-  return (
-    <div className='flex items-center justify-center w-screen min-h-screen bg-slate-50'>
-      <h1 className='text-[70px]'>Opps..... </h1>
-    </div>
-  );
-}
